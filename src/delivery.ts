45112db91d014e772c26340f83ad2942fc740ef0
Luke Dary <luke@bullseye.ltd>
2022-12-15 14:06:13 -0500

    Server

--- src/delivery.ts
+++ src/delivery.ts
@@ -4,7 +4,25 @@
  */
 
 /*
-
-<https://chapeaux.io>
+\
+CLI args
+    - path/p = base path to local assets
 
 */
+
+import { serve } from "https://deno.land/std@0.165.0/http/server.ts";
+
+const port = 8080;
+
+const handler = (request: Request): Response => {
+    const loc = new URL(request.url);
+    console.log(`Prot:${loc.protocol}, Host:${loc.host}, Path:${loc.pathname}, Search:${[...loc.searchParams.entries()]}`);
+  const body = `Your user-agent is:\n\n${
+    request.headers.get("user-agent") ?? "Unknown"
+  }`;
+
+  return new Response(body, { status: 200 });
+};
+
+console.log(`HTTP webserver running. Access it at: http://localhost:8080/`);
+await serve(handler, { port });
